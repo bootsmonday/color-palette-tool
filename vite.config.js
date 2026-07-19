8629928fa093f5b2fb38a6f6d81d8ef0dcdb7639
matt.vrtis <matt.vrtis@gmail.com>
2026-07-19 12:50:56 -0400

    commenting out vite react plugin

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 // If you're using React:
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [tailwindcss()],
